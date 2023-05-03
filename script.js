92bd26008e138c80d8a7eac83b1474cd3902cb8e
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-05-02 20:44:56 -0700

    added min and max values for password generation

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Assignment code here
-var length = prompt("how many characters would you like in your password? (8-128)");
-var includeUpper = confirm("Would you like upper case letters in your password?");
-var includeNumbers = confirm("Would you like to include numbers in your password?");
-var	includeSpecial = confirm("Would you like to include special characters in your password?");
+// set variables to the type of variable desired
+var length = 0;
+var includeUpper = false;
+var includeNumbers = false;
+var	includeSpecial = false;
 
 var lettersLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var lettersUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -14,6 +15,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  length = prompt("how many characters would you like in your password? (8-128)");
+  if(length < 8 || length > 128){
+    return;
+  }
+  includeUpper = confirm("Would you like upper case letters in your password?");
+  includeNumbers = confirm("Would you like to include numbers in your password?");
+  includeSpecial = confirm("Would you like to include special characters in your password?");
   var password = generatePassword(includeUpper,includeNumbers,includeSpecial);
   var passwordText = document.querySelector("#password");
 
