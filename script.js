59e6d6529d26737b7da56e58a2d36b96eb6833fa
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-05-02 20:22:50 -0700

    added full functionality to random password generator criteria

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // Assignment code here
 var length = prompt("how many characters would you like in your password? (8-128)");
-var includeUpper = confirm("Would you like upper case letters in your password?")
+var includeUpper = confirm("Would you like upper case letters in your password?");
+var includeNumbers = confirm("Would you like to include numbers in your password?");
+var	includeSpecial = confirm("Would you like to include special characters in your password?");
 
 var lettersLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var lettersUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lettersUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var special = ["!","@","#","$","%","^","&","*","?"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  if(includeUpper == false){
-  var password = generatePasswordlowercase();}
+  var password = generatePassword(includeUpper,includeNumbers,includeSpecial);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -20,6 +23,30 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword(includeUpperBoolean, includeNumbersBoolean, includeSpecialBoolean){
+  var sampleArray = lettersLower;
+  if(includeUpperBoolean === true){
+    console.log("includeUpperBoolean is true");
+    sampleArray = sampleArray.concat(lettersUpper);
+  }
+  if(includeNumbersBoolean === true){
+    sampleArray = sampleArray.concat(numbers);
+  }
+  if(includeSpecialBoolean === true){
+    sampleArray = sampleArray.concat(special);
+  }
+  
+  var password = "";
+  var counter = length;
+  var sampleLength = sampleArray.length;
+  while(counter > 0){
+    var index = Math.floor(Math.random()*sampleLength);
+    password += sampleArray[index];
+    counter--;
+  }
+  return password;
+
+}
 
 function generatePasswordlowercase() {
   var password = "";
