7267b9ae6eb98eebd776d58345256af1c410fd69
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-05-02 18:19:27 -0700

    defined some variables and created basic password generation prototype

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment code here
-
+var length = prompt("how many characters would you like in your password? (8-128)");
+var lettersLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lettersUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +17,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  var password = "";
+  var counter = length;
+  var sampleLength = lettersLower.length;
+  while(counter > 0){
+    var index = Math.floor(Math.random()*sampleLength);
+    password += lettersLower[index];
+    counter--;
+  }
+  return password;
+}
