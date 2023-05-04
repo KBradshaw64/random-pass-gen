1a7bff47cb9914260562960aed88bd576b4af877
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-05-03 17:02:08 -0700

    changed array variables to const

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ var includeUpper = false;
 var includeNumbers = false;
 var	includeSpecial = false;
 
-var lettersLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var lettersUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var special = ["!","@","#","$","%","^","&","*","?"];
+const lettersLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const lettersUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const numbers = ["0","1","2","3","4","5","6","7","8","9"];
+const special = ["!","@","#","$","%","^","&","*","?"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -38,9 +38,11 @@ function generatePassword(includeUpperBoolean, includeNumbersBoolean, includeSpe
     sampleArray = sampleArray.concat(lettersUpper);
   }
   if(includeNumbersBoolean === true){
+    console.log("includeNumbersBoolean is true")
     sampleArray = sampleArray.concat(numbers);
   }
   if(includeSpecialBoolean === true){
+    console.log("includeSpecialBoolean is true")
     sampleArray = sampleArray.concat(special);
   }
   
